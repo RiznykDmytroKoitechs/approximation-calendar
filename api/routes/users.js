f7c9d4a0fcd507e593c9e154b1b1923fec64a9de
RiznykDmytroKoitechs <115988767+RiznykDmytroKoitechs@users.noreply.github.com>
2022-11-09 14:56:41 +0200

    init

---
+++ api/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+const AuthService = require('../services/authServise')
+var router = express.Router();
+
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
